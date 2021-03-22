c14a98f68b52a73106fd9566b3ac13e12da438fc
Kumar Prafull <eprafull@gmail.com>
2021-03-22 20:04:13 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(()=> {
+  console.log('run function')
+  return 4
+})
+
+function minusCount() {
+  setCount(prevCount => prevCount-1)
+}
+
+function plusCount() {
+  setCount(prevCount => prevCount+1)
+}
+
   return (
     <>
-    <button>  -  </button>
-    <span>0</span>
-    <button>  +  </button>    
+    <button onClick={minusCount}>  -  </button>
+    <span> {count} </span>
+    <button onClick={plusCount}>  +  </button>    
     </>
   );
 }
